082045d5f6b562cdf7cbcd156ae8fcb760051d6a
jbazann <joa7.baz@gmail.com>
2025-08-16 21:37:08 -0300

    Inline events.js exports to make adding new events less annoying.

--- src/lib/events.js
+++ src/lib/events.js
@@ -1,114 +1,90 @@
 import { log } from "./log"
 import { t } from "./t"
 
-export {
-    dispatch,
-    newInputEvent,
-    newPrintEvent,
-    newScrollToBottomEvent,
-    newScrollToPageEvent,
-    newFetchPartialEvent,
-    newLocationEvent,
-    newHistoryPushEvent,
-    newCommandEvent,
-    newTtyFocusEvent,
-    newRewireEvent,
-    newSetAttributeEvent,
-    newErrorMessageEvent,
-    newLanguageSwitchEvent,
-    newCheckRedirectEvent,
-    newNoticeEvent,
-    newPrintPrefacedEvent,
-    newCatImgEvent,
-    newPrepareCatImgEvent,
-    newClsEvent,
-    newHrEvent
-}
-
 const DEFAULT_DISPATCH_OPTIONS = {
     target: document
 }
-function dispatch(event, opt = DEFAULT_DISPATCH_OPTIONS) {
+export function dispatch(event, opt = DEFAULT_DISPATCH_OPTIONS) {
     opt.target.dispatchEvent(_log(event))
 }
 
-function newInputEvent(input, line) {
+export function newInputEvent(input, line) {
     return new CustomEvent('tty:input', {detail: {input, line}})
 }
 
-function newPrintEvent(line) {
+export function newPrintEvent(line) {
     return new CustomEvent('tty:print', {detail: {line}})
 }
 
-function newScrollToBottomEvent() {
+export function newScrollToBottomEvent() {
     return new CustomEvent('tty:scroll:bottom')
 }
 
-function newScrollToPageEvent() {
+export function newScrollToPageEvent() {
     return new CustomEvent('tty:scroll:page')
 }
 
-function newFetchPartialEvent(detail) {
+export function newFetchPartialEvent(detail) {
     return new CustomEvent('tty:partial', {detail})
 }
 
-function newLocationEvent(path) {
+export function newLocationEvent(path) {
     return new CustomEvent('tty:location', {detail: {path}})
 }
 
-function newHistoryPushEvent() {
+export function newHistoryPushEvent() {
     return new CustomEvent('tty:history:push')
 }
 
-function newCommandEvent(cmd) {
+export function newCommandEvent(cmd) {
     return new CustomEvent('tty:command', {detail: {cmd}})
 }
 
-function newTtyFocusEvent() {
+export function newTtyFocusEvent() {
     return new CustomEvent('tty:focus')
 }
 
-function newRewireEvent(kind = 'page') {
+export function newRewireEvent(kind = 'page') {
     return new CustomEvent(`tty:rewire:${kind}`)
 }
 
-function newSetAttributeEvent(attribute,value,target = document.documentElement) {
+export function newSetAttributeEvent(attribute,value,target = document.documentElement) {
     return new CustomEvent('set:attribute',{detail: {attribute,value,target}})
 }
 
-function newErrorMessageEvent(msg) {
+export function newErrorMessageEvent(msg) {
     return new CustomEvent('tty:print:error',{detail: {line: msg}})
 }
 
-function newLanguageSwitchEvent() {
+export function newLanguageSwitchEvent() {
     return new CustomEvent('tty:lang:switch')
 }
 
-function newCheckRedirectEvent() {
+export function newCheckRedirectEvent() {
     return new CustomEvent('tty:check-redirect')
 }
 
-function newNoticeEvent(msg) {
+export function newNoticeEvent(msg) {
     return new CustomEvent('tty:print:notice', {detail: {line: msg}})
 }
 
-function newPrintPrefacedEvent(preface,line) {
+export function newPrintPrefacedEvent(preface,line) {
     return new CustomEvent('tty:print:preface', {detail: {preface,line}})
 }
 
-function newHrEvent() {
+export function newHrEvent() {
     return new CustomEvent('tty:print:hr')
 }
 
-function newCatImgEvent(url) {
+export function newCatImgEvent(url) {
     return new CustomEvent('tty:print:cat', {detail: {url, alt: t('cmd.cat.alt')}})
 }
 
-function newPrepareCatImgEvent(urls) {
+export function newPrepareCatImgEvent(urls) {
     return new CustomEvent('tty:prepare:print:cat', {detail: {urls}})
 }
 
-function newClsEvent() {
+export function newClsEvent() {
     return new CustomEvent('tty:cls')
 }
 
